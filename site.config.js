0449e7f74040fe65f160e7f3cf2d548e5cb047f1
Yihui He <yihuihe.yh@gmail.com>
2022-02-20 22:24:14 -0800

    config

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'bd32b787e47149f38941174a9c6846b6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Yihui He',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Yihui He',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Yihui He',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
